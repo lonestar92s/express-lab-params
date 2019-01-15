037c7e64a5cc4534b8337a25bfc2c5935bee91a9
Andrew Aluko <aluko17@Andrews-MacBook-Air.local>
2019-01-15 15:28:04 -0600

    Commit 3: Magic 8 Ball express application created.

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.get('/tip/:total/:tipPercent',(request,response)=> {
 
 })
 
+
+let responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+let randomResponse = (Math.floor(Math.random() * responses.length)) 
+
+
+app.get('/magic/:question',(request,response)=> {
+	response.send(`<p>${request.params.question}</p><p>${responses[randomResponse]}`)
+})
+
+
+
 app.listen(8000)
 
 
