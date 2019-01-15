864501119c73d68f1830c147f3d14b21a4efb61f
Andrew Aluko <aluko17@Andrews-MacBook-Air.local>
2019-01-15 15:11:25 -0600

    Commit 2: Tip Calculator express application created.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 let express = require('express')
 let app = express()
-
+let tipCalc = function(total,tipPercent){
+	return total * (tipPercent/100)
+}
 
 app.get('/greeting/:name',(request,response)=> {
 	response.send(`Hello there ${request.params.name}, how's it going?`)
 
 })
 
+
+app.get('/tip/:total/:tipPercent',(request,response)=> {
+	response.send(`Your tip is ${tipCalc(request.params.total,request.params.tipPercent)} dollars. `)
+
+})
+
 app.listen(8000)
+
+
+
+
+
+
+
+
